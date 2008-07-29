51c12d8122178a6ae476ba8e4a5c502c2affed99
Peter Eckersley <pde@eff.org>
2008-07-29 16:20:07 +0000

    Make tests/HashDump work again.

--- tests/HashDump.py
+++ tests/HashDump.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python2.5
 
 import sys
-sys.path.append('../src/common')
-from switzerland.client import Packet
 from binascii import hexlify
 
-# HashDump.py dumpfile private_ip public_ip [firewalled peer_firewalled]
+from switzerland.client import Packet
+
+# HashDump.py dumpfile private_ip public_ip [firewalled] [peer_firewalled]
 
 class FakeConfig:
     def __init__(self):
         self.private_ip = sys.argv[2]
+        self.pcap_datalink = 1
 class FakeLink:
     def __init__(self):
         self.firewalled = "firewalled" in sys.argv[3:]
@@ -21,14 +22,14 @@ class FakeAlice:
         self.fm=FakeFM()
 class FakeFM():
     def __init__(self):
-        self.ipids = {}
+        self.ip_ids = {}
 
 raw_packet = file(sys.argv[1], "r")
 data = raw_packet.read()
 raw_packet.close()
 
 p = Packet.Packet(0, data, FakeAlice())
-print "IPID is", hexlify(p.ipid)
+print "IPID is", hexlify(p.ip_id)
 p.peer_firewalled = "peer_firewalled" in sys.argv[3:]
 hash = p.get_hash()
 print "hash: ", hexlify(hash)
