00d20ac2e41bae1ec483c43acf52f0a8b84cd80d
Jered Wierzbicki <jeredw@gmail.com>
2009-05-05 21:02:01 +0000

    get rid of  sys.exit() call in postinstall script, which causes the distutils-generated installer program to die

--- win32_postinstall.py
+++ win32_postinstall.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python
 
 import distutils.sysconfig
-import sys
+import os, sys
+
+def fix_path():
+    script_dir = distutils.sysconfig.get_config_var('prefix') + os.sep + 'scripts'
+    environment = 'SYSTEM\CurrentControlSet\Control\Session Manager\Environment'
+    
+    k = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, environment, 0, winreg.KEY_WRITE | winreg.KEY_READ)
+    
+    (path, type) = winreg.QueryValueEx(k, 'Path')
+    
+    if not script_dir in path.split(os.pathsep):
+        new_path = ""
+        if path.endswith(os.pathsep): # sometimes windows likes to stick an extra ; at the end
+            new_path = path + script_dir + os.pathsep
+        else: # other times it's sane...
+            new_path = path + os.pathsep + script_dir
+        print "adding "+script_dir+" to your windows %PATH% so switzerland can find its packet sniffer"
+        winreg.SetValueEx(k, 'Path', 0, type, new_path)      
+        (path, type) = winreg.QueryValueEx(k, 'Path')
+        if not script_dir in path.split(os.pathsep):
+            print "... well, I tried, but it didn't work.  "\
+                    "put " + script_dir + " in your path to run switzerland."
+        else:
+            print "telling open applications that your path changed (so you don't have to reboot)"
+            rc, dwReturnValue = win32gui.SendMessageTimeout(win32con.HWND_BROADCAST,\
+                    win32con.WM_SETTINGCHANGE,\
+                    0, "Environment", win32con.SMTO_ABORTIFHUNG, 5000)
+            print "ok, I updated your path, you should be good to go"
+    
+    else:
+        print script_dir + " is already in your windows path, should be good to go"
 
 loaded_everything = True
 try:
@@ -12,38 +42,7 @@ except ImportError, e:
     print "Missing python win32 extensions, please install: "\
           "http://sourceforge.net/project/showfiles.php?group_id=78018"
 
-if loaded_everything == False:
-    print "not doing post-install steps because not all dependencies were available\n"
-    sys.exit(2)
-
-import os
-
-script_dir = distutils.sysconfig.get_config_var('prefix') + os.sep + 'scripts'
-
-environment = 'SYSTEM\CurrentControlSet\Control\Session Manager\Environment'
-
-k = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, environment, 0, winreg.KEY_WRITE | winreg.KEY_READ)
-
-(path, type) = winreg.QueryValueEx(k, 'Path')
-    
-if not script_dir in path.split(os.pathsep):
-    new_path = ""
-    if path.endswith(os.pathsep): # sometimes windows likes to stick an extra ; at the end
-        new_path = path + script_dir + os.pathsep
-    else: # other times it's sane...
-        new_path = path + os.pathsep + script_dir
-    print "adding "+script_dir+" to your windows %PATH% so switzerland can find its packet sniffer"
-    winreg.SetValueEx(k, 'Path', 0, type, new_path)      
-    (path, type) = winreg.QueryValueEx(k, 'Path')
-    if not script_dir in path.split(os.pathsep):
-        print "... well, I tried, but it didn't work.  "\
-              "put " + script_dir + " in your path to run switzerland."
-    else:
-        print "telling open applications that your path changed (so you don't have to reboot)"
-        rc, dwReturnValue = win32gui.SendMessageTimeout(win32con.HWND_BROADCAST,\
-            win32con.WM_SETTINGCHANGE,\
-            0, "Environment", win32con.SMTO_ABORTIFHUNG, 5000)
-        print "ok, I updated your path, you should be good to go"
-            
+if loaded_everything == True:
+    fix_path()
 else:
-      print script_dir + " is already in your windows path, should be good to go"
+    print "not doing post-install steps because not all dependencies were available\n"
