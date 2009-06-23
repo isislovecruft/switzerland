d2eeb7e2868845e55e67f4272a235b49c6667879
Caden Howell <caden@cadenhowell.com>
2009-06-23 02:02:39 +0000

    Created "Fake" API file for testing WebGUI with no Switzerland connection Added comment pseudocode for main graph in WebGUI.py, started code for compiling packet data

---
+++ switzerland/client/AliceAPIFake.py
@@ -0,0 +1,177 @@
+
+import time
+import random
+
+def ClientConfig(self):
+    '''A factory function for the API's xAliceConfig objects.'''
+    return xAliceConfig()
+
+def connectServer(self, config):
+    return xAlice(config)
+    
+class xAliceConfig:
+    def __init__(self):
+        #self.actual_config = AliceConfig()
+        self.extract_options_for_api()
+        # Fake for no AliceConfig
+        self.option_hash = dict()
+
+    def tweakable_options(self):
+        return self.tweakable_options
+
+    def immutable_options(self):
+        return self.immutable_options
+
+    def extract_options_for_api(self):
+        """
+        AliceAPI will feed these to code that may want to know about options
+        it can change
+        """
+        self.option_hash["host"] = "switzerland.eff.org"
+        self.option_hash["port"] = 7778
+        self.option_hash["interface"] = None
+        self.option_hash["skew"] = 0.0
+        self.option_hash["log_level"] = logging.DEBUG
+        self.option_hash["seriousness"] = 0
+        self.option_hash["do_cleaning"] = True
+        self.option_hash["use_ntp"] = True
+        self.option_hash["filter_packets"] = True
+        self.option_hash["force_public_ip"] = False
+        self.option_hash["force_private_ip"] = False
+        self.option_hash["packet_buffer_size"] = 25000
+        self.option_hash["quiet"] = False         
+                 
+        tweakable = self.tweakable_options = [("log_level", string),
+        ("seriousness", int),
+        ("do_cleaning", bool)]
+        immutable = self.immutable_options = [("host", ip),
+        ("port", int),
+        ("interface", string),
+        ("skew", float),
+        ("use_ntp", bool),
+        ("filter_packets", bool),
+        ("force_public_ip", bool),
+        ("force_private_ip", bool),
+        ("packet_buffer_size", int)
+        ("quiet", bool)]
+
+
+    def set_option(self,option,value):
+        if self.option_hash[option] != None:
+            self.option_hash[option] = value
+            
+        
+    def get_option(self,option):
+        return self.option_hash[option]
+
+class xAlice:
+    def __init__(self, config):
+        self.start_time = time.time()
+        self.message_count = 0
+        self.last_message_time = time.time()
+        self.fake_peers = list()
+        self.fake_peers.append(xPeer(None))
+        self.fake_peers[0].fake_flows.append(xFlow(None))
+        self.fake_peers[0].fake_flows.append(xFlow(None))
+        self.fake_peers.append(xPeer(None))
+        self.fake_peers[1].fake_flows.append(xFlow(None))
+        self.fake_peers[1].fake_flows.append(xFlow(None))
+        self.fake_peers.append(xPeer(None))
+        self.fake_peers[2].fake_flows.append(xFlow(None))
+        self.fake_peers[2].fake_flows.append(xFlow(None))
+        
+        assert isinstance(config, xAliceConfig)
+        #self.actual_alice = Alice(config.actual_config)
+    def disconnect(self):
+        pass
+    def get_server_info(self):
+        temp_time = time.time() - self.start_time
+        temp_last = time.time() - self.last_message_time
+        info = {'hostname': 'switzerland.eff.org', 
+        'ip': '1.2.3.4', 
+        'connection time': temp_time, 
+        'message_count': self.message_count, 
+        'last_message': temp_last}
+
+        return info
+    def get_client_info(self):
+        info = {'public_ip': '5.6.7.8', 
+        'private_ip': '192.168.1.2', 
+        'network interface': 'tcp', 
+        'ntp method': 'unknown', 
+        'clock dispersion': 0.0}
+        return info
+    
+    def get_peers(self):
+        return self.fake_peers
+
+ 
+class xPeer:
+    def __init__(self, actual_peer):
+        self.actual_peer = actual_peer
+        self.fake_flows = list()
+    def traceroute(self):
+        return "Here is the route to this peer"
+    def active_flows(self):
+        return self.fake_flows
+    def old_flows(self):
+        return []
+
+
+class xFlow:
+    def __init__(self, actual_flow):
+        self.actual_flow = actual_flow
+        #self.flow_tuple = self.actual_flow.summary()[3]
+        self.flow_tuple = ( \
+            str(random.randint(1,255)) + '.' + str(random.randint(1,255)) + '.' + str(random.randint(1,255)) + '.' + str(random.randint(1,255)), 
+            random.randint(1025,65000), 
+            str(random.randint(1,255)) + '.' + str(random.randint(1,255)) + '.' + str(random.randint(1,255)) + '.' + str(random.randint(1,255)), 
+            random.randint(1025,65000), 
+            random.choice(('tcp','ip')))
+        self.last_update = time.time()
+        self.last_packet_count_time = time.time()
+        self.last_byte_count_time = time.time()
+        self.last_dropped_time = time.time()
+        self.last_injected_time = time.time()
+        self.last_modified_time = time.time()
+
+    def get_pair(self):
+        '''Return the matching flow in the other direction, or None if there
+        isn\'t one.'''
+        return None
+
+    def is_active(self):
+        return True
+    
+    def rand_packet_list(self, starttime, mult=1):
+        packets = list()
+        tcount = self.last_packet_count_time()
+        tnow = time.time()
+        while (tcount < tnow) :
+            packets.append((tcount, xPacket()))
+            tcount = tcount + random.random() * mult
+        
+    
+    def get_new_packet_count(self):
+        return random.randint(1000,5000)
+        return packets
+
+    def get_new_byte_count(self):
+        return random.randint(10000,50000)
+
+    def get_new_dropped(self):
+        packets = rand_packet_list(self.last_dropped_time)
+        self.last_dropped_time = time.time()
+
+    def get_new_injected(self):
+        packets = rand_packet_list(self.last_injected_time)
+        self.last_injected_time = time.time()
+ 
+    def get_new_modified(self):
+        packets = rand_packet_list(self.last_modified_time)
+        self.last_modified_time = time.time()
+
+class xPacket:
+    def __init__(self):
+        pass
+
--- switzerland/client/WebGUI.py
+++ switzerland/client/WebGUI.py
@@ -1,22 +1,143 @@
 
-import threading
 import web
+#from AliceAPI import xAlice, xAliceConfig, xPeer, xFlow
+from AliceAPIFake import xAlice, xAliceConfig, xPeer, xFlow
 
-class WebGUI(threading.Thread):
+class WebGUI():
     """ Run a GUI with monitoring features as a local web server """
 
-    def __init__(self, parent):
-        threading.Thread.__init__(self)
-        self.parent = parent
+    def __init__(self):
+        self.x_alice_config = xAliceConfig()
+        self.x_alice = xAlice.connect_server(self.x_alice_config)
+        self.packet_data = dict()
         self.urls = (
             '/', 'index', 
-            '', 'index')
-
-    def run(self):
+            '', 'index',
+            'config[/]?', 'config')
+        
+    def main(self):
         self.app = web.application(self.urls, globals())
         self.app.run()
 
 
+class info_utility:
+    def display_client_info(self):
+        html = '''<table>'''
+        for key, value in WebGUI.x_alice.get_client_info():
+            html = html + '''<tr><td>''' + key + '''</td><td>''' 
+            html = html + value + '''</td></tr>'''
+        html = html + '''</table>'''
+        return html
+    
+    def display_server_info(self):
+        html = '''<table>'''
+        for key, value in WebGUI.x_alice.get_server_info():
+            html = html + '''<tr><td>''' + key + '''</td><td>''' 
+            html = html + value + '''</td></tr>'''
+        html = html + '''</table>'''
+        return html
+    
+    def display_peer_list(self):
+        html = '''<table>'''
+        for peer in WebGUI.x_alice.get_peers():
+            for flow_tuple in WebGUI.x_alice.active_flows():
+                html = html + '''<tr><td>''' + + flow_tuple[0] + + '''</td>'''
+                html = html + '''<td>''' + flow_tuple[1] + '''</td>''' 
+                html = html + '''<td>''' + flow_tuple[2] + '''</td>''' 
+                html = html + '''<td>''' + flow_tuple[3] + '''</td></tr>'''
+        html = html + '''</table>'''
+        return html
+        
+    # flow = xFlow object
+    # packet_type = dropped, injected, modified
+    def make_histogram(self, flow, bin_size, packet_type):
+        # Count packets into bins
+        # Return histogram
+    
+    # Pass in all flows and get bin size used if all of these were
+    # plotted on same graph
+    # flows = list of xFlows objects
+    def get_hist_bin_size(self, flows):
+        # Find min and max timestamp
+        # Divide by number of bins (~100?)
+        # Adjust to nice round number
+        # Return bin size (in minutes) 
+        
+    def make_graph_data(self, histogram, units):
+        # For each bin in histogram
+        # Get x from histogram bin
+        # Get y from histogram value
+        # Return canvas-formatted graph data (for line drawing)
+        
+    def make_graph(self):
+        info_utility.update_packet_data()
+        # Get bin size for all flows
+        # For each flow considered
+        # Make a histogram
+        # Make graph data
+        # Make legend
+        # Finally, plot to canvas
+        
+    def update_packet_data(self):
+        # For each active flow
+        for p in WebGUI.x_alice.get_peers:
+            for f in p.active_flows:
+                # If flow does not exist in dictionary object, add
+                flow_ip = f.flow_tuple[0] + ":" + str(f.flow_tuple[1]) + "::" + f.flow_tuple[2] + ":" + str(f.flow_tuple[3])
+                if self.packet_data[flow_ip] :
+                    pass
+                else
+                    self.packet_data[flow_ip] = dict()
+                    self.packet_data[flow_ip]['dropped'] = list()
+                    self.packet_data[flow_ip]['injected'] = list()
+                    self.packet_data[flow_ip]['modified'] = list()
+                    self.packet_data[flow_ip]['total count'] = 0
+                
+                # Each active flow has 4 lists of packets: dropped, injected, 
+                # modified, total count
+                self.packet_data[flow_ip]['dropped'].extend(f.get_new_dropped())
+                self.packet_data[flow_ip]['injected'].extend(f.get_new_injected())
+                self.packet_data[flow_ip]['modified'].extend(f.get_new_modified())    
+                self.packet_data[flow_ip]['total count']  = self.packet_data[flow_ip]['total count'] + get_new_packet_count() 
+
+    
 class index:
 	def GET(self):
-		return "This would be the front page of the application"
+        page = '''<html><head><title>Switzerland</title></head>
+        <body>'''
+        page = page + info_utility.display_client_info()
+        page = page + info_utility.display_server_info()
+        
+        page = '''
+            <canvas id="cnvPacketGraph" width="600" height="400">
+            </canvas>
+        '''
+        page = page + make_graph()
+        page = page + '''
+            </body>
+            </html>
+        '''
+        return page
+
+# List mutable configuration parameters, allow to change
+class config:
+    def GET(self):  
+        page = '''<p>Tweakable Options</p>
+                    <form name="frmMutableOpt" id="frmMutableOpt"><table>'''
+                    
+        for opt in WebGUI.x_alice_config.tweakable_options
+            page = page + '''<tr><td>''' + opt + '''</td>'''
+            page = page + '''<td><input name="''' + opt +  '''"''' 
+            page = page + ''' value="''' + WebGUI.x_alice_config.get_option(opt) 
+            page = page + '''"/></td></tr>'''
+        
+        page = page + '''</table><br/>
+                    <input type="submit" value="Save changes"/></form>
+                    <p>Immutable Options</p>
+                    <table>'''
+                    
+        for opt in WebGUI.x_alice_config.immutable_options
+            page = page + '''<tr><td>''' + opt + '''</td>'''
+            page = page + '''<td>''' + WebGUI.x_alice_config.get_option(opt) + '''</td></tr>'''
+         
+        return page
