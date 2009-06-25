e34cb52a2542f2fae272a6e5753718ec203942ad
Caden Howell <caden@cadenhowell.com>
2009-06-25 01:32:08 +0000

    Removed script compiler errors, now trying to get canvas graph to show up.

--- switzerland/client/WebGUI.py
+++ switzerland/client/WebGUI.py
@@ -1,32 +1,21 @@
-
+#!/usr/bin/env python
 import web
-import decimal
+import time
+import math
 
 #from AliceAPI import xAlice, xAliceConfig, xPeer, xFlow
 from AliceAPIFake import xAlice, xAliceConfig, xPeer, xFlow
 
-class WebGUI():
-    """ Run a GUI with monitoring features as a local web server """
-
-    def __init__(self):
-        self.x_alice_config = xAliceConfig()
-        self.x_alice = xAlice.connect_server(self.x_alice_config)
-        self.packet_data = dict()
-        self.urls = (
-            '/', 'index', 
-            '', 'index',
-            'config[/]?', 'config')
-        
-    def main(self):
-        self.app = web.application(self.urls, globals())
-        self.app.run()
+singleton_webgui = None
 
 
 class line_graph:
     def __init__(   self, 
-                    graph_xbins=100, 
+                    canvas_id="cid",
+                    canvas_context="jg",
                     width=600, 
                     height=400,
+                    graph_xbins=100, 
                     graph_ybins=50):  
                     
         self.graph_xbins = graph_xbins
@@ -37,21 +26,26 @@ class line_graph:
         self.max_timestamp = None
         self.min_timestamp = None
         self.bin_size = None
-        self.packet_data = dict()
+        
         self.gui_flows = None
+        # JavaScript canvas context name
+        self.canvas_context = canvas_context
+        # HTML element ID of canvas element
+        self.canvas_id = canvas_id
     
     # flow = xFlow object
     # packet_type = dropped, injected, modified
     def make_histogram(self, packet_list):
         histogram = list()
-        for i in graph_xbins:
+        for i in range(0,self.graph_xbins):
             histogram.append(0)
             
         # Count packets into bins
         for packet_ts in [p[0] for p in packet_list]:
             i =  packet_ts - self.min_timestamp
             i = int(i/self.bin_size)
-            histogram[i] = histogram + 1
+            print "bin number", i
+            histogram[i] = histogram[i] + 1
             
         # Return histogram
         return histogram
@@ -64,198 +58,246 @@ class line_graph:
         
         # Find min and max timestamp
         all_timestamps = list()
-        for ip, fl in self.gui_flows :
-            ts_list = [p[0] for p in self.packet_data[ip]['dropped']]
+        all_packcount = list()
+        for ip in self.gui_flows :
+            ts_list = [p[0] for p in singleton_webgui.packet_data[ip]['dropped']]
             # Rather than concatenating ALL the timestamps, we
             # only need the mins and maxes.
             all_timestamps.extend((min(ts_list), max(ts_list)))
-            ts_list = [p[0] for p in self.packet_data[ip]['injected']]
+            ts_list = [p[0] for p in singleton_webgui.packet_data[ip]['injected']]
             all_timestamps.extend((min(ts_list), max(ts_list)))
-            ts_list = [p[0] for p in self.packet_data[ip]['modified']]
+            ts_list = [p[0] for p in singleton_webgui.packet_data[ip]['modified']]
             all_timestamps.extend((min(ts_list), max(ts_list)))
+            print "total count", singleton_webgui.packet_data[ip]['total count']
+            all_packcount.extend([p[1] for p in singleton_webgui.packet_data[ip]['total count']])
         
-        self.max_timestamp = compute_utility.find_max(all_timestamps)
-        self.min_timestamp = compute_utility.find_min(all_timestamps)
+        self.max_timestamp = max(all_timestamps)
+        self.min_timestamp = min(all_timestamps)
+        print "max ts", self.max_timestamp, "min ts", self.min_timestamp
+        self.y_hist_max = max(all_packcount)
         range_timestamp = self.max_timestamp - self.min_timestamp
 
         # Divide by number of bins (~100?)
-        self.bin_size = range_timestamp / graph_xbins
+        self.bin_size = range_timestamp / self.graph_xbins
                 
         # Adjust to nice round number
-        self.bin_size = round(self.bin_size,int(-math.floor(math.log10(self.bin_size)))) 
+        binlog = math.log10(self.bin_size)
+        self.bin_size = round(self.bin_size + (5 * 10**(binlog-1)) ,int(-math.floor(binlog))) 
         
+        print "rough bin size", str(range_timestamp / self.graph_xbins), "bin size", self.bin_size
         # Return bin size (in seconds) 
         return self.bin_size
     
     def make_graph_data(self, name, histogram):
-        # Set scaling factor
-        # For each bin in histogram
+        
         i = 0
-        xhtml = '''var x_''' + name + ''' = new Array('''
-        yhtml = '''var y_''' + name + ''' = new Array('''
+        html = self.canvas_context + ".beginPath();\n"
+        
+        # For each bin in histogram
         for b in histogram:
             # Get x from histogram bin
             # Get y from histogram value
-            xhtml = xhtml + (i * (self.width/self.graph_xbins)) + ''','''
-            yhtml = yhtml + b + ''','''
+            x = str(int(i * (self.width/self.graph_xbins))) 
+            y = str(600 - int(b * (self.height/self.graph_ybins))) 
+            if i == 0:
+                html = html + self.canvas_context + ".moveTo(" + x + "," + y + ");\n"
+            else:
+                html = html + self.canvas_context + ".lineTo(" + x + "," + y + ");\n"    
             i = i + 1
-        
-        xhtml = xhtml[:-1]
-        yhtml = yhtml[:-1]
-        xhtml = xhtml + ''');'''
-        yhtml = yhtml + ''');'''
-        html = xhtml + "\n" + yhtml + "\n"
+            
+        html = html + self.canvas_context + ".closePath();\n\n"       
+        html = html + self.canvas_context + ".stroke();\n\n"        
+
         # Return canvas-formatted graph data (for line drawing)
         return html
     
+    def make_total_pkt_count_graph_data(self, name, tuples):
+        i = 0
+        xhtml = "var x_" + name + " = new Array("
+        yhtml = "var y_" + name + " = new Array("
+        for t in tuples:
+            xhtml = xhtml + str(t[0] * (self.width/self.graph_xbins)) + ","
+            yhtml = yhtml + str(t[1] * (self.y_hist_max/self.graph_ybins)) + ","
+        
+        xhtml = xhtml[:-1]
+        yhtml = yhtml[:-1]
+        xhtml = xhtml + ");"
+        yhtml = yhtml + ");"
+        html = xhtml + "\n" + yhtml + "\n"    
+        
     def update_packet_data(self):
         # For each active flow
-        for p in WebGUI.x_alice.get_peers:
-            for f in p.active_flows:
+        peers = singleton_webgui.x_alice.get_peers()
+        for p in peers:
+            flows = p.active_flows()
+            for f in flows:
                 # If flow does not exist in dictionary object, add
-                flow_ip = f.flow_tuple[0] + ":" + str(f.flow_tuple[1]) + "::" + f.flow_tuple[2] + ":" + str(f.flow_tuple[3])
-                if self.packet_data[flow_ip] :
+                flow_ip = str(f.flow_tuple[0]) + ":" + str(f.flow_tuple[1]) + "::" + str(f.flow_tuple[2]) + ":" + str(f.flow_tuple[3])
+                if singleton_webgui.packet_data.get(flow_ip) :
                     pass
                 else:
-                    self.packet_data[flow_ip] = dict()
-                    self.packet_data[flow_ip]['dropped'] = list()
-                    self.packet_data[flow_ip]['injected'] = list()
-                    self.packet_data[flow_ip]['modified'] = list()
-                    self.packet_data[flow_ip]['total count'] = 0
+                    singleton_webgui.packet_data[flow_ip] = dict()
+                    singleton_webgui.packet_data[flow_ip]['dropped'] = list()
+                    singleton_webgui.packet_data[flow_ip]['injected'] = list()
+                    singleton_webgui.packet_data[flow_ip]['modified'] = list()
+                    singleton_webgui.packet_data[flow_ip]['total count'] = list()
+                
+                print flow_ip
+                
                 
                 # Each active flow has 4 lists of packets: dropped, injected, 
                 # modified, total count
-                self.packet_data[flow_ip]['dropped'].extend(f.get_new_dropped())
-                self.packet_data[flow_ip]['injected'].extend(f.get_new_injected())
-                self.packet_data[flow_ip]['modified'].extend(f.get_new_modified())    
-                self.packet_data[flow_ip]['total count']  = self.packet_data[flow_ip]['total count'] + get_new_packet_count() 
+                singleton_webgui.packet_data[flow_ip]['dropped'].extend(f.get_new_dropped())
+                singleton_webgui.packet_data[flow_ip]['injected'].extend(f.get_new_injected())
+                singleton_webgui.packet_data[flow_ip]['modified'].extend(f.get_new_modified())    
+                singleton_webgui.packet_data[flow_ip]['total count'].extend([(time.time(),  f.get_new_packet_count()) ])
+                
+        
     
-    def make_graph(self, name, width, height):
-        info_utility.update_packet_data()
+    def make_graph(self):
+        self.update_packet_data()
         # Update which flows we care about
         # For now, all of them.  Will update to those selected by GUI
         self.gui_flows = dict()
-        
-        for p in WebGUI.x_alice.get_peers:
-            for f in p.active_flows:
+        peers = singleton_webgui.x_alice.get_peers()
+        for p in peers:
+            flows = p.active_flows()
+            for f in flows:
                 flow_ip = f.flow_tuple[0] + ":" + str(f.flow_tuple[1]) + "::" + f.flow_tuple[2] + ":" + str(f.flow_tuple[3])
                 self.gui_flows[flow_ip] = f
         
         # Get bin size for all flows
-        get_hist_bin_size()
+        self.get_hist_bin_size()
         self.histograms = dict()
         
         graph_data_html = ""
         line_names = dict()
         # For each flow considered
-        for ip, fl in self.gui_flows:
+        for ip in self.gui_flows:
             # Make a histogram
             self.histograms[ip] = dict()
-            self.histograms[ip]['dropped'] = make_histogram(self.packet_data[ip]['dropped'])
-            self.histograms[ip]['injected'] = make_histogram(self.packet_data[ip]['injected'])
-            self.histograms[ip]['modified'] = make_histogram(self.packet_data[ip]['modified'])
+            self.histograms[ip]['dropped'] = self.make_histogram(singleton_webgui.packet_data[ip]['dropped'])
+            self.histograms[ip]['injected'] = self.make_histogram(singleton_webgui.packet_data[ip]['injected'])
+            self.histograms[ip]['modified'] = self.make_histogram(singleton_webgui.packet_data[ip]['modified'])
         
         # Get maximum y value (# of packets)
-        self.y_hist_max = compute_utility.find_max(self.histograms)
+        self.y_hist_max = find_max(self.histograms)
         
-        for ip, fl in self.gui_flows:
+        for ip in self.gui_flows:
             line_name = ip.replace(":","_")
+            line_name = line_name.replace(".","_")
             # Make graph data
-            graph_data_html = graph_data_html + make_graph_data(line_name + "_dr", self.histogram[ip]['dropped'], width, height)
-            graph_data_html = graph_data_html + make_graph_data(line_name + "_in", self.histogram[ip]['injected'], width, height)
-            graph_data_html = graph_data_html + make_graph_data(line_name + "_mo", self.histogram[ip]['modified'], width, height)
+            graph_data_html = graph_data_html + self.make_graph_data(line_name + "_dr", self.histograms[ip]['dropped'])
+            graph_data_html = graph_data_html + self.make_graph_data(line_name + "_in", self.histograms[ip]['injected'])
+            graph_data_html = graph_data_html + self.make_graph_data(line_name + "_mo", self.histograms[ip]['modified'])
             line_names[line_name] = line_name
             
+        html = '''
+<canvas id="''' + self.canvas_id + '''" width="''' + str(self.width+10) + '''" height="''' + str(self.height+10) + '''">       
+    Canvas is not supported.
+</canvas>
+<script type="text/javascript">
+<!--
+    function drawgraph_''' + self.canvas_id + '''() {
+        var canvas_''' + self.canvas_id + ''' = document.getElementById("''' + self.canvas_id + '''");
+        if (canvas_''' + self.canvas_id + '''.getContext) {            
+            var ''' + self.canvas_context + ''' = canvas_''' + self.canvas_id + '''.getContext('2d');
+''' + graph_data_html + '''  
+        } else {
+            alert('You need Safari or Firefox 1.5+ to see this demo.');
+        }
+    }
+    
+    drawgraph_''' + self.canvas_id + '''();
+//-->
+</script>
+        '''    
+        # Finally, plot to canvas 
+        return html   
+
+
+
+def find_max( struct):
+    if isinstance(struct, dict):
+        return find_max(struct.values())
+    if isinstance(struct, list) or isinstance(struct, sequence) or isinstance(struct, set):
+        item = struct[0]
+        if isinstance(item, str) or isinstance(item, int):
+            return max(struct)
+        else:
+            if len(struct) == 1:
+                return find_max(item)
+            else:
+                m2 = find_max(struct[1:])
+                m1 = find_max(item)
+                if m1 >= m2:
+                    return m1
+                else:
+                    return m2
             
-        # Finally, plot to canvas    
-
-
-class compute_utility:
-    def find_max( struct):
-        if isinstance(struct, dict):
-            return find_max(struct.values())
-        if isinstance(struct, list) or isinstance(struct, sequence) or isinstance(struct, set):
-            item = struct[0]
-            if isinstance(item, str) or isinstance(item, int):
-                return max(struct)
+def find_min( struct):
+    if isinstance(struct, dict):
+        return find_min(struct.values())
+    if isinstance(struct, list) or isinstance(struct, sequence) or isinstance(struct, set):
+        item = struct[0]
+        if isinstance(item, str) or isinstance(item, int):
+            return min(struct)
+        else:
+            if len(struct) == 1:
+                return find_min(item)
             else:
-                if len(struct) == 1:
-                    return find_max(item)
+                m2 = find_min(struct[1:])
+                m1 = find_min(item)
+                if m1 >= m2:
+                    return m1
                 else:
-                    m2 = find_max(struct[1:])
-                    m1 = find_max(item)
-                    if m1 >= m2:
-                        return m1
-                    else:
-                        return m2
-                
-    def find_min( struct):
-        if isinstance(struct, dict):
-            return find_min(struct.values())
-        if isinstance(struct, list) or isinstance(struct, sequence) or isinstance(struct, set):
-            item = struct[0]
-            if isinstance(item, str) or isinstance(item, int):
-                return min(struct)
-            else:
-                if len(struct) == 1:
-                    return find_min(item)
-                else:
-                    m2 = find_min(struct[1:])
-                    m1 = find_min(item)
-                    if m1 >= m2:
-                        return m1
-                    else:
-                        return m2    
+                    return m2    
        
 class info_utility:
     
     def display_client_info(self):
         html = '''<table>'''
-        for key, value in WebGUI.x_alice.get_client_info():
+        info_hash = singleton_webgui.x_alice.get_client_info()
+        for key in info_hash:
             html = html + '''<tr><td>''' + key + '''</td><td>''' 
-            html = html + value + '''</td></tr>'''
+            html = html + str(info_hash[key]) + '''</td></tr>'''
         html = html + '''</table>'''
         return html
     
     def display_server_info(self):
         html = '''<table>'''
-        for key, value in WebGUI.x_alice.get_server_info():
+        info_hash = singleton_webgui.x_alice.get_server_info()
+        for key in info_hash:
             html = html + '''<tr><td>''' + key + '''</td><td>''' 
-            html = html + value + '''</td></tr>'''
+            html = html + str(info_hash[key])  + '''</td></tr>'''
         html = html + '''</table>'''
         return html
     
     def display_peer_list(self):
         html = '''<table>'''
-        for peer in WebGUI.x_alice.get_peers():
-            for flow_tuple in WebGUI.x_alice.active_flows():
-                html = html + '''<tr><td>''' + + flow_tuple[0] + + '''</td>'''
-                html = html + '''<td>''' + flow_tuple[1] + '''</td>''' 
-                html = html + '''<td>''' + flow_tuple[2] + '''</td>''' 
-                html = html + '''<td>''' + flow_tuple[3] + '''</td></tr>'''
+        for peer in singleton_webgui.x_alice.get_peers():
+            for f in peer.active_flows():
+                html = html + '''<tr><td>''' + str(f.flow_tuple[0]) + '''</td>'''
+                html = html + '''<td>''' + str(f.flow_tuple[1]) + '''</td>''' 
+                html = html + '''<td>''' + str(f.flow_tuple[2]) + '''</td>''' 
+                html = html + '''<td>''' + str(f.flow_tuple[3]) + '''</td></tr>'''
         html = html + '''</table>'''
         return html
-        
-
 
     
 class index:
-	def GET(self):
-        page = '''<html><head><title>Switzerland</title></head>
-        <body>'''
-        page = page + info_utility.display_client_info()
-        page = page + info_utility.display_server_info()
+    def GET(self):
+        page = '''<html><head><title>Switzerland</title></head><body>'''
+        i = info_utility()
+        page = page + i.display_client_info()
+        page = page + i.display_server_info()
+        page = page + i.display_peer_list()
         
-        width = 600
-        height = 400
-        page = '''
-            <canvas id="cnvPacketGraph" width="''' + width + '''" height="''' + height + '''">
-            </canvas>
-        '''
-        page = page + make_graph("cnvPacketGraph", width, height)
+        graph = line_graph()
+        page = page + graph.make_graph()
         page = page + '''
-            </body>
-            </html>
+        </body>
+        </html>
         '''
         return page
 
@@ -265,7 +307,7 @@ class config:
         page = '''<p>Tweakable Options</p>
                     <form name="frmMutableOpt" id="frmMutableOpt"><table>'''
                     
-        for opt in WebGUI.x_alice_config.tweakable_options
+        for opt in singleton_webgui.x_alice_config.tweakable_options:
             page = page + '''<tr><td>''' + opt + '''</td>'''
             page = page + '''<td><input name="''' + opt +  '''"''' 
             page = page + ''' value="''' + WebGUI.x_alice_config.get_option(opt) 
@@ -276,8 +318,30 @@ class config:
                     <p>Immutable Options</p>
                     <table>'''
                     
-        for opt in WebGUI.x_alice_config.immutable_options
+        for opt in WebGUI.x_alice_config.immutable_options:
             page = page + '''<tr><td>''' + opt + '''</td>'''
             page = page + '''<td>''' + WebGUI.x_alice_config.get_option(opt) + '''</td></tr>'''
          
         return page
+    
+
+class WebGUI():
+    """ Run a GUI with monitoring features as a local web server """
+
+    def __init__(self):
+        self.x_alice_config = xAliceConfig()
+        self.x_alice = xAlice(self.x_alice_config)
+        self.packet_data = dict()
+        self.urls = (
+            '/', 'index', 
+            '', 'index',
+            'config[/]?', 'config')
+        
+    def main(self):
+        self.app = web.application(self.urls, globals())
+        self.app.run()
+
+
+if __name__ == "__main__":
+    singleton_webgui = WebGUI()
+    singleton_webgui.main()
